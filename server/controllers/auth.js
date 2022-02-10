4e8ccdc1c6735da2177a97e43b30c4c0471acee7
ChrisCluney <ccluney@gmail.com>
2022-02-10 17:15:49 -0600

    Initial commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcryptjs')
+const users = []
+
+module.exports = {
+    login: (req, res) => {
+      
+      console.log(req.body)
+      const { username, password } = req.body
+      for (let i = 0; i < users.length; i++) {
+        const existing = bcrypt.compareSync(password, users[i].password)
+        if (users[i].username === username && existing) {
+          const userCopy = {...users[i]}
+          delete userCopy.password
+          console.log(('Logging In User'))
+          res.status(200).send(users[i])
+          return
+        }
+      }
+      res.status(400).send("User not found.")
+    },
+    register: (req, res) => {
+      console.log(req.body)
+      const {username, email, firstName, lastName, password} = req.body
+      
+      let salt = bcrypt.genSaltSync(5)
+      const passwordHash = bcrypt.hashSync(password, salt)
+
+
+      const newUser = {
+        username: username,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        password: passwordHash
+
+      }
+        
+        users.push(newUser)
+        let userCopy = {...newUser}
+        delete userCopy.password
+        res.status(200).send(req.body)
+    }
+}
